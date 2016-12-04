2e10506084ad773cbbeb0f1887dcedd8dd873a48
sage.xue <job.xueqi@gmail.com>
2016-12-04 13:16:57 +0800

    连接池客户端操作Invoke,封装资源获取和还原的过程

---
+++ zookeeper-driver-client/src/main/java/org/sagesource/zookeeperdriver/client/pool/ClientPoolInvoke.java
@@ -0,0 +1,38 @@
+package org.sagesource.zookeeperdriver.client.pool;
+
+import org.sagesource.zookeeperdriver.client.wrapper.ZkClientWrapper;
+
+/**
+ * <p>连接池客户端操作Invoke,封装资源获取和还原的过程</p>
+ * <pre>
+ *     author      Sage XueQi
+ *     date        2016/12/4
+ *     email       job.xueqi@gmail.com
+ * </pre>
+ */
+public class ClientPoolInvoke {
+
+	/**
+	 * invoke执行方法,可以在里面封装链接的获取和归还
+	 *
+	 * @param clientKey
+	 * @param processor
+	 * @param <T>       返回值对象
+	 * @return
+	 */
+	public static <T> T invoke(String clientKey, PoolInvokeProcessor<T> processor) throws Exception {
+		ZkClientWrapper client = ClientPoolOperation.getClientFromPool(clientKey);
+		T               result = processor.processor(client);
+		ClientPoolOperation.returnClientToPool(client);
+		return result;
+	}
+
+	/**
+	 * properties接口 在实现接口的时候 直接使用client即可 会自动注进去
+	 *
+	 * @param <T>
+	 */
+	public interface PoolInvokeProcessor<T> {
+		public T processor(ZkClientWrapper client);
+	}
+}
