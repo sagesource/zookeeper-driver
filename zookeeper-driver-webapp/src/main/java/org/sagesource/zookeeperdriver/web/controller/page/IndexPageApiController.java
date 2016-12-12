20e48ce534c8ddc4c0601f6ebd237432c811c0f5
sage.xue` <job.xueqi@gmail.com>
2016-12-12 13:18:40 +0800

    添加首页

--- zookeeper-driver-webapp/src/main/java/org/sagesource/zookeeperdriver/web/controller/page/IndexPageApiController.java
+++ zookeeper-driver-webapp/src/main/java/org/sagesource/zookeeperdriver/web/controller/page/IndexPageApiController.java
@@ -26,7 +26,7 @@ public class IndexPageApiController extends BaseApiController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
-		return "index/index";
+		return "front/index";
 	}
 
 }
