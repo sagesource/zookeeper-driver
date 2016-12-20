20f47627caa5ff8b8b5a1b2d3420432a9b460bdf
sage.xue` <job.xueqi@gmail.com>
2016-12-20 11:19:52 +0800

    集成测试环境添加springfox

--- zookeeper-driver-webapp/src/main/webapp/WEB-INF/static/front/index/js/scripts.js
+++ zookeeper-driver-webapp/src/main/webapp/WEB-INF/static/front/index/js/scripts.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function () {
     // Fullscreen background
     $.backstretch([
-        "static/front/index/img/backgrounds/2.jpg"
-        , "static/front/index/img/backgrounds/3.jpg"
-        , "static/front/index/img/backgrounds/1.jpg"
+        baseContext + "/static/front/index/img/backgrounds/2.jpg"
+        , baseContext + "/static/front/index/img/backgrounds/3.jpg"
+        , baseContext + "/static/front/index/img/backgrounds/1.jpg"
     ], {duration: 3000, fade: 750});
 
     $('#btn_connect').click(function () {
